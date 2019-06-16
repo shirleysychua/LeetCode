e045c364536500fddf5f7c07f0597e63db94619f
shirleysychua <shirleysychua@gmail.com>
2019-06-16 21:11:43 +0800

    Improve Solution to Two Sum problem

--- LeetCode/easy/TwoSum.swift
+++ LeetCode/easy/TwoSum.swift
@@ -5,14 +5,18 @@ import UIKit
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var map = [Int:Int]()
     
-    for index1 in 0..<nums.count {
-        for index2 in index1+1..<nums.count {
-            if(nums[index1] + nums[index2] == target) {
-                return [index1, index2]
-            }
+    for index in 0..<nums.count {
+        let diff: Int = target - nums[index]
+        if(map[nums[index]] == nil) {
+            // store difference as key so we can easily check later if this difference is found in succeeding indices
+            map[diff] = index
+        } else {
+            return [map[nums[index]]!, index]
         }
     }
+    
     return []
 }
 
