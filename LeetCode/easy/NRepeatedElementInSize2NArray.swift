1b51fd307656b1fa87d9967249d1480970243932
shirleysychua <shirleysychua@gmail.com>
2019-06-16 20:40:52 +0800

    Comment problem description

--- LeetCode/easy/NRepeatedElementInSize2NArray.swift
+++ LeetCode/easy/NRepeatedElementInSize2NArray.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 // In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+//
+//Return the element repeated N times.
 
-Return the element repeated N times.
 func repeatedNTimes(_ A: [Int]) -> Int {
     var parsed = Set<Int>()
     var answer : Int = 0
